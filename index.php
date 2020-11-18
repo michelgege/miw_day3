598aea15d7dab24fa819d8aa30eefcf97f355844
michelgege <michelgege@pop-os.localdomain>
2020-11-18 11:43:07 +0100

    basic structure

--- index.php
+++ index.php
@@ -20,29 +20,62 @@ if (!empty($_GET['delivery']) and $_GET['delivery'] === "sent") {
     // echo "OK c'est envoyé..";
     $body_class = "delivery_sent";
 }
+?>
 
-
-
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="fr">
+    <!--
+    ______HEAD______________
+    [x] html lang
+    [x] meta charset
+    [x] meta description
+    [x] meta viewport
+    [x] balise title
+    [] favicon 
 
-<head>
+    ______UX__________________________
+    [] augmenter surface d'appuis des <a>
+    [] effet hover sur tout ce qui est clickable
+    [] label for
+    [] champs required avec element visuel qui le montre
+    [] placeholder
+    [] ancres
 
-    <title></title>
+    ______UI__________________________
+    [] fallback color sur les images
+    
+    ______BONNES PRATIQUES_____________
+    [] virer au maximum les div/classes inutiles
+    [] virer imbrications css inutiles
+    [] commenter css/html
 
-    <style type="text/css">
-        .delivery_sent {
-            background-color: green;
-        }
-    </style>
-
-</head>
+    ______OPTIMISATION_________________
+    [] utilisaton SVG sprite sheet / font icons
+    [] compression / crop images 
+    [] un seul fichier CSS / JS
+    [] images cropées si elles ne s'affichent pas en entier (par exemple sur mobile)
+    [] progressive jpeg : https://coding.tools/progressive-jpeg
+    
+    ______SEO / ACCESSIBILITE / AUTRES____________
+    [] svg role="img", svg <title>, <desc> ??
+    [] balises alt des images
+    [] balise nav
+    [] ul li pour les menus 
+    [] attributs width et height sur les images
+    -->
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Site d'inscription a la newsletter de l'université">
+        <title>Inscription newsletter</title>
+        <link rel="stylesheet" href="/dist/css/main.css">
+    </head>
 <body class="<?= $body_class ?>">
 
-<form action="#" method="post">
-    <input type="email" name="email" placeholder="Ton email de star..." required/>
-    <input type="submit" value="Submit"/>
-</form>
+    <form action="#" method="post">
+        <input type="email" name="email" placeholder="Ton email de star..." required/>
+        <input type="submit" value="Submit"/>
+    </form>
 
 </body>
 </html>
